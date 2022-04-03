e22fa17bc8777917d3e17a082609846ecf5c3988
Jason Benjoya <jasonbenjoya@gmail.com>
2022-04-02 21:12:30 -0500

    added db config file

--- backend/config.js
+++ backend/config.js
@@ -2,6 +2,12 @@
 
 const PORT = +process.env.PORT || 3001;
 
+const getDatabaseURI = (dbName) => {
+  return process.env.NODE_ENV === "test"
+    ? `${dbName}_test`
+    : process.env.DATABASE_URL || `${dbName}`;
+};
 module.exports = {
+  getDatabaseURI,
   PORT,
 };
---
+++ backend/db.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/** Database setup **/
+
+const { Client } = require("pg");
+const { getDatabaseURI } = require("./config");
+
+let dbName; //set with desired name of database for project
+
+// TODO- uncomment once above has been set
+// const db = new Client({
+//   connectionString: getDatabaseURI(dbName),
+// });
+// db.connect();
+
+module.exports = db;
